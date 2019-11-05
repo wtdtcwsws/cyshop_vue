8e1081d3559221134a5f979ac50d674f3a3d60a7
wutong <602232262@qq.com>
2019-11-05 22:21:58 +0800

    RadioGroup

--- src/components/InputGroup.vue
+++ src/components/InputGroup.vue
@@ -1,10 +1,10 @@
 <template>
-    <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
-        <el-form>
-            <el-form-item label="名称">
-                <el-input v-model="formLabelAlign.name"></el-input>
+    <el-form :rules="rules" ref="formName" :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+            <el-form-item :label="labelText" prop="name">
+
+                <el-input v-if="" name="name" v-model="formLabelAlign.name"  :type="type" :placeholder="placeholder" show-password></el-input>
             </el-form-item>
-        </el-form>
+        <!--<el-button type="primary" @click="submitForm" >提交</el-button>-->
     </el-form>
 
 </template>
@@ -12,17 +12,69 @@
 <script>
     export default {
         name:'InputGroup',
+        // mounted(){
+        //     this.rules.name={
+        //      required: true, message: this.validRules.message, trigger: this.validRules.trigger
+        //     }
+        // },
         data() {
             return {
                 labelPosition: 'right',
                 formLabelAlign: {
                     name: ''
                 }
+                ,
+                rules: {
+                    name:[
+                        { required: true, message: this.validRules.message, trigger: this.validRules.trigger }
+                    ]
+                }
             };
+        },
+        methods: {
+            submitForm() {
+                let vm = this;
+                vm.$refs['formName'].validate((valid) => {
+                    if (valid) {
+                        alert('submit!');
+                    } else {
+                        console.log('error submit!!');
+                        return false;
+                    }
+                });
+            }
+            // ,
+            // loadRules(){
+            //     // rules.name={
+            //     //        required: true, message: "122221", trigger: 'blur'
+            //     // }
+            //     this.labelPosition="left"
+            // }
+        },
+        props:{
+            labelText:String,
+            type:String,
+            placeholder:String,
+            eye:{
+                type:Boolean,
+                default:false
+            },
+            validRules:{
+                    type:Object,
+                    default(){
+                        return{
+                            message:"必填",
+                            trigger:'blur'
+                        }
+                    }
+
+            }
         }
     }
 </script>
 
 <style scoped>
-
+    .el-form{
+        width: 460px;
+    }
 </style>
---
+++ src/components/RadioGroup.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <el-form-item :label=fItem.label >
+            <el-radio :name=fItem.name v-for='(n,i) in items' v-model="radio" :label="i">{{n.name}}</el-radio>
+        </el-form-item>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "RadioGroup",
+        data () {
+            return {
+                radio: 0
+            };
+        },
+        props:{
+            items:Array,
+            fItem:{
+                type:Object
+                ,
+                default(){
+                    return{
+                        name:"name",
+                        label:"标签"
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
