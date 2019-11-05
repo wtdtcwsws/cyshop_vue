22438008227e3905d6f97ebdff733fe93f1a8d2c
BradRowe <xiaozhi_411@163.com>
2019-11-05 12:35:16 +0800

    添加按钮、面包屑导航、返回按钮上传

--- src/router.js
+++ src/router.js
@@ -4,67 +4,72 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    // {
-    //   path: '/',
-    //   name: 'home',
-    //   component: Home
-    // },
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    // }, {
-    //       path: '/table',
-    //       name: 'table',
-    //       component: () => import(/* webpackChunkName: "about" */ './views/Table.vue')
-    //   }
-      {
-          path: '/',
-          redirect:'/login'
-      },
-      {
-          name:'login',
-          path:'/login',
-          component:()=>import('./views/Login')
-      },
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        // {
+        //   path: '/',
+        //   name: 'home',
+        //   component: Home
+        // },
+        // {
+        //   path: '/about',
+        //   name: 'about',
+        //   // route level code-splitting
+        //   // this generates a separate chunk (about.[hash].js) for this route
+        //   // which is lazy-loaded when the route is visited.
+        //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        // }, {
+        //       path: '/table',
+        //       name: 'table',
+        //       component: () => import(/* webpackChunkName: "about" */ './views/Table.vue')
+        //   }
+        {
+            path: '/',
+            redirect: '/login'
+        },
+        {
+            name: 'login',
+            path: '/login',
+            component: () => import('./views/Login')
+        },
 
-      {
-          name:'Index',
-          path:'/index',
-          component:()=>import('./views/Main'),
-          children:[
-              {
-                  name:'home',
-                  path:'/home',
-                  component:()=>import('./views/Home')
-              },
-              {
-                  name:'about',
-                  path:'/about',
-                  component:()=>import('./views/About')
-              },
-              {
-                  name:'table',
-                  path:'/table',
-                  component:()=>import('./views/Table')
-              },{
-                  name:'wutong',
-                  path:'/wutong',
-                  component:()=>import('./views/WuTong')
-              },
-              {
-                  name:'table',
-                  path:'/table',
-                  component:()=>import('./views/Table')
-              }
-          ]
-      }
+        {
+            name: 'Index',
+            path: '/index',
+            component: () => import('./views/Main'),
+            children: [
+                {
+                    name: 'home',
+                    path: '/home',
+                    component: () => import('./views/Home')
+                },
+                {
+                    name: 'about',
+                    path: '/about',
+                    component: () => import('./views/About')
+                },
+                {
+                    name: 'table',
+                    path: '/table',
+                    component: () => import('./views/Table')
+                }, {
+                    name: 'liuzhi',
+                    path: '/liuzhi',
+                    component: () => import('./views/liuzhi')
+                },
+                {
+                    name: 'wutong',
+                    path: '/wutong',
+                    component: () => import('./views/WuTong')
+                },
+                {
+                    name: 'table',
+                    path: '/table',
+                    component: () => import('./views/Table')
+                }
+            ]
+        }
 
-  ]
+    ]
 })
---
+++ src/views/liuzhi.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="liuzhi">
+
+        <abc></abc>
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import abc from '@/components/AddButton--添加按钮.vue'
+
+    export default {
+        name: 'liuzhi',
+        components: {
+            abc
+        }
+    }
+</script>
