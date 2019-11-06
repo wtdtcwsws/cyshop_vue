ce5f25e288712ceb0c4cf4c46b20600bdb92a57f
luohai <541545580@qq.com>
2019-11-06 19:43:55 +0800

    day3Luo

--- src/router.js
+++ src/router.js
@@ -4,45 +4,118 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-      {
-          path: '/',
-          redirect:'/login'
-      },
-      {
-          name:'login',
-          path:'/login',
-          component:()=>import('./views/Login')
-      },
-      {
-          name:'Main',
-          path:'/main',
-          component:()=>import('./views/Main'),
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        // {
+        //   path: '/',
+        //   name: 'home',
+        //   component: Home
+        // },
+        // {
+        //   path: '/about',
+        //   name: 'about',
+        //   // route level code-splitting
+        //   // this generates a separate chunk (about.[hash].js) for this route
+        //   // which is lazy-loaded when the route is visited.
+        //   component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        // }, {
+        //       path: '/table',
+        //       name: 'table',
+        //       component: () => import(/* webpackChunkName: "about" */ './views/Table.vue')
+        //   }
+        {
+            path: '/',
+            redirect: '/login'
+        },
+        {
+            name: 'login',
+            path: '/login',
+            component: () => import('./views/Login')
+        },
+        {
+            name: 'Index',
+            path: '/index',
+            component: () => import('./views/Main'),
+            children: [
+                {
+                    name: 'home',
+                    path: '/home',
+                    component: () => import('./views/Home')
+                },
+                {
+                    name: 'about',
+                    path: '/about',
+                    component: () => import('./views/About')
+                },
+                {
+                    name: 'table',
+                    path: '/table',
+                    component: () => import('./views/Table')
+                }
+                // , {
+                //     name: 'liuzhi',
+                //     path: '/liuzhi',
+                //     component: () => import('./views/liuzhi')
+                // },
+                // {
+                //     name: 'wutong',
+                //     path: '/wutong',
+                //     component: () => import('./views/WuTong')
+                // },
+                // {
+                //     name:'zhy',
+                //     path:'/zhy',
+                //     component:()=>import('./views/Zhy')
+                // }
+                ,
+                {
+                    name: 'table',
+                    path: '/table',
+                    component: () => import('./views/Table')
+                },
+                {
+                    name: 'orderList',
+                    path: '/orders/orderList',
+                    component: () => import('./views/orders/OrderList')
+                },
+                {
+                    name: 'orderDetails',
+                    path: '/orders/orderDetails',
+                    component: () => import('./views/orders/OrderDetails')
+                },
+                {
+                    name: 'refundList',
+                    path: '/orders/refundList',
+                    component: () => import('./views/orders/RefundList')
+                },
+                {
+                    name: 'refundDetails',
+                    path: '/orders/refundDetails',
+                    component: () => import('./views/orders/RefundDetails')
+                },
+                {
+                    name: 'catalog',
+                    path: '/goods/catalog',
+                    component: () => import('./views/goods/Catalog')
+                },
+                {
+                    name:'MemberLevel',
+                    path:'/level',
+                    component:()=>import('./views/member/MemberLevel')
+                },
+                {
+                    name:'MemberList',
+                    path:'/member',
+                    component:()=>import('./views/member/MemberList')
+                },
+                {
+                    name:'CommodityStore',
+                    path:'/store',
+                    component:()=>import('./views/goods/CommodityStore'),
 
-      },
-      {
-          name:'About',
-          path:'/about',
-          component:()=>import('./views/About'),
+                }]
+        }
 
-      },
-      {
-          name:'MemberLevel',
-          path:'/level',
-          component:()=>import('./views/member/MemberLevel'),
-
-      },
-      {
-          name:'MemberList',
-          path:'/member',
-          component:()=>import('./views/member/MemberList')
-      },
-      {
-          name:'CommodityStore',
-          path:'/store',
-          component:()=>import('./views/CommodityStore'),
-
-      }]
+    ]
 })
