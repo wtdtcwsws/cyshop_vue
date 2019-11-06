b3af87ff6c8408f9ef9f581433492e71372c7d62
qq1301951370 <1301951370@qq.com>
2019-11-06 14:23:18 +0800

    测试导航跳转 添加角色管理员页面文件

---
+++ src/components/role/RoleList.vue
@@ -0,0 +1,59 @@
+<template>
+    <div id="RoleList">
+        <!--角色权限列表-->
+        <el-table
+                :data="tableData"
+                style="width: 100%">
+            <el-table-column
+                    prop="date"
+                    label="日期"
+                    width="180">
+            </el-table-column>
+            <el-table-column
+                    prop="name"
+                    label="姓名"
+                    width="180">
+            </el-table-column>
+            <el-table-column
+                    prop="address"
+                    label="地址">
+            </el-table-column>
+        </el-table>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "RoleList",
+        data() {
+            return {
+
+            }
+        }
+    }
+
+    data.put('return {\n' +
+        '    tableData: [{\n' +
+        '        date: \'2016-05-02\',\n' +
+        '        name: \'王小虎\',\n' +
+        '        address: \'上海市普陀区金沙江路 1518 弄\'\n' +
+        '    }, {\n' +
+        '        date: \'2016-05-04\',\n' +
+        '        name: \'王小虎\',\n' +
+        '        address: \'上海市普陀区金沙江路 1517 弄\'\n' +
+        '    }, {\n' +
+        '        date: \'2016-05-01\',\n' +
+        '        name: \'王小虎\',\n' +
+        '        address: \'上海市普陀区金沙江路 1519 弄\'\n' +
+        '    }, {\n' +
+        '        date: \'2016-05-03\',\n' +
+        '        name: \'王小虎\',\n' +
+        '        address: \'上海市普陀区金沙江路 1516 弄\'\n' +
+        '    }]\n' +
+        '}');
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -56,11 +56,11 @@
 
                         <el-menu-item-group>
                             <span slot="title">分组一</span>
-                            <el-menu-item index="1-3">选项3</el-menu-item>
+                            <el-menu-item index="1-3"><router-link to="/table">表格</router-link></el-menu-item>
                         </el-menu-item-group>
 
                         <el-submenu index="1-4">
-                            <span slot="title">选项4</span>
+                            <span slot="title"><router-link to="/home">首页</router-link></span>
                             <el-menu-item index="1-4-1">选项1</el-menu-item>
                         </el-submenu>
                     </el-submenu>
