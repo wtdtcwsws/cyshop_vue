09bdfee89434254006bfd5d81befa98db3ccc249
575257868 <575257868@qq.com>
2019-11-06 09:02:40 +0800

    增加了card mnubar Navigation 更改了Main里面增加了两个标签

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,13 +1,16 @@
 <template>
     <el-container>
-        <el-header style="background-color: rgb(33, 33, 33)"></el-header>
+        <el-header class="header" style="background-color: rgb(84, 92, 100)">
+            <navigation></navigation>
+        </el-header>
         <el-container>
-            <el-aside style="background-color: rgb(84, 92, 100)">
-                <nvabar></nvabar>
+            <el-aside style="background-color: rgb(205, 255, 255)">
+                <mvabar></mvabar>
             </el-aside>
             <el-container>
                 <el-main>
-                    <router-view/>
+                    <card></card>
+                    <router-view />
                 </el-main>
                 <el-footer></el-footer>
             </el-container>
@@ -16,18 +19,25 @@
 </template>
 
 <script>
-    import nvabar from "@/components/NavMenu"
-
-    export default {
-        name: "Main",
-        components:{
-            nvabar:nvabar
-        }
+import mvabar from "@/components/MnuBar";
+import navigation from "@/components/Navigation";
+import card from "@/components/Card";
+export default {
+    name: "Main",
+    components: {
+        mvabar: mvabar,
+        navigation: navigation,
+        card: card
     }
+};
 </script>
 
 <style scoped>
-    .el-container{
-        height: 100%;
-    }
+.el-container {
+    height: 100%;
+}
+.header {
+    margin: 0;
+    padding: 0;
+}
 </style>
